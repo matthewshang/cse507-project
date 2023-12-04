f0133f70951b3f4f24b506400e88e744adae19db
tenzinhl <tenzinhl@uw.edu>
2023-12-04 15:30:18 -0800

    Initial GUI implementation
    Can use input square to select a query and then submit it + view
    results.

--- golz3.py
+++ golz3.py
@@ -2,10 +2,17 @@ import itertools
 from z3 import *
 
 # A Slice is a rectangular grid representing Life state at a specific time.
-Slice = list[list[ExprRef]]
+Slice = list[list[ArithRef]]
 
 def make_life(solver: Solver, grid_size: int, time_steps: int) -> list[Slice]:
-    """Create a (time steps) x (grid size) x (grid size) array of symbolic variables.
+    """
+    Create a (time steps) x (grid size) x (grid size) array of symbolic variables
+    representing a Game of Life board of size grid_size across time_steps time
+    steps.
+
+    The variables in each time step are constrained such that slice at time i
+    leads to the board state at time i + 1 (where we assume all cells outside of
+    the region of interest are dead).
 
     Variables are named following the convention:
     "s_{t}_{i}_{j}", where t is the time step, i is the row, and j is the column.
@@ -16,7 +23,8 @@ def make_life(solver: Solver, grid_size: int, time_steps: int) -> list[Slice]:
         time_steps (int): The number of time-steps to generate
 
     Returns:
-        list[Slice]: _description_
+        list[Slice]: A list of Slice's, where list[i] represents the i'th timestep (larger i ==> later
+          in time). Use this to reference the symbolic variables when constraining or accessing.
     """
     # (tenzin): Note at some point we can potentially replace `Int`'s with `Bool`'s and instead use Z3
     # Pseudo-boolean constraints for counting boolean cardinality:
@@ -32,6 +40,7 @@ def make_life(solver: Solver, grid_size: int, time_steps: int) -> list[Slice]:
                 continue
 
             # Life rules.
+            # `count` symbolically counts the number of alive neighbors cell[i][j] has.
             count = 0
             for di, dj in itertools.product(range(-1, 2), range(-1, 2)):
                 if di == dj == 0:
@@ -65,4 +74,21 @@ def constrain(solver: Solver, s: Slice, on: set[tuple[int, int]]):
             else:
                 solver.add(s[i][j] == 0)
 
+def model_to_python(model: ModelRef, state: list[Slice]) -> list[list[list[int]]]:
+    """
+    Convert a model for sequence of Game of Life states to a native python
+    3D-list of integers where each int represents cell state at given time.
 
+    list[time][row][col] gives the cell state of cell at row,col at given timestep.
+
+    Args:
+        model (ModelRef): Model of states.
+        state (list[Slice]): State variables organized s.t. state[time][row][col]
+            gives reference for Z3 variable representing cell at given time, row, and
+            column.
+
+    Returns:
+        list[list[list[int]]]: list[time][row][col] gives the cell state of cell
+            at row,col at given timestep, but in Python native form :sunglasses:.
+    """
+    return [[[model[var].as_long() for var in row] for row in slice] for slice in state] # type: ignore
--- gui.py
+++ gui.py
@@ -6,39 +6,56 @@ import tkinter
 from tkinter import ttk
 import itertools
 from z3 import *
+import golz3
 
 # Side length in px of cells when drawn to board.
 CELL_SIZE = 30
 
+
 class Cell:
     """
     A single cell in the Game of Life board.
+
+    States:
+    - 0 = dead
+    - 1 = alive/selected in Board
     """
-    def __init__(self, alive=0):
-        self.alive = alive
+
+    def __init__(self, state=0):
+        self.state = state
+
+    def toggle(self):
+        self.state = 0 if self.state else 1
+
 
 # Board offset from top and left side of canvas so that lines draw.
 BOARD_OFFSET = 10
+
 
 class Board:
     """
     The Game of Life board.
     """
-    def __init__(self, canvas: tkinter.Canvas, size: int):
+
+    def __init__(self, canvas: tkinter.Canvas, size: int, bind_callbacks=True):
         """Create a new Game of Life Board.
 
         Args:
             canvas (Canvas): Canvas to use for drawing life board. This Board object
                 takes ownership of the Board and will clear/draw to it.
             size (int): side length of grid in terms of number of cells.
+            bind_callbacks (bool): if true, then bind callbacks for this Board (so it
+                can be interacted with by clicking).
         """
         self.canvas = canvas
         self.size: int = size
         # 2D array of Cell objects representing the state of the board.
-        self.board: list[list[Cell]] = [[Cell(0) for col in range(size)] for row in range(size)]
+        self.board: list[list[Cell]] = [
+            [Cell(0) for col in range(size)] for row in range(size)]
 
-        self.bind_callbacks()
-        
+        if bind_callbacks:
+            self.bind_callbacks()
+
         canvas.delete("all")
         self.draw()
 
@@ -52,14 +69,14 @@ class Board:
             for col in range(self.size):
                 # Mysterious OFFSETs are to get grid lines to all draw (otherwise top and left side
                 # of grid are cut off)
-                fill = "gray26" if self.board[row][col].alive else "white"
+                fill = "gray26" if self.board[row][col].state else "white"
                 self.canvas.create_rectangle(
                     BOARD_OFFSET + col * CELL_SIZE,
                     BOARD_OFFSET + row * CELL_SIZE,
                     BOARD_OFFSET + (col + 1) * CELL_SIZE,
                     BOARD_OFFSET + (row + 1) * CELL_SIZE,
                     fill=fill)
-    
+
     def bind_callbacks(self):
         """
         Bind callbacks for clicking etc. to the provided canvas.
@@ -94,7 +111,64 @@ class Board:
 
         # Otherwise: invalid offset.
         return None
-    
+
+    def get_cell_state(self, row: int, col: int) -> int:
+        """
+        Return the state of the cell at (row, col) in this board.
+
+        Raises an Invalid Argument Exception if row/col are out of bounds.
+
+        Args:
+            row (int): row of cell.
+            col (int): column of cell.
+
+        Returns:
+            int: the state of cell at (row, col).
+        """
+        if not (0 <= row < self.size and 0 <= col < self.size):
+            raise ValueError(
+                f"Row {row} and column {col} not within grid (grid size={self.size})")
+
+        return self.board[row][col].state
+
+    def set_cell_state(self, row: int, col: int, state: int):
+        """
+        Set the state of the Cell at (row, col) to the specified state. For
+        changes to be reflected on board, board must be redrawn.
+
+        Raises an Invalid Argument Exception if row/col are out of bounds.
+
+        Args:
+            row (int): row of cell.
+            col (int): column of cell.
+            state (int): new state for cell.
+        """
+        if not (0 <= row < self.size and 0 <= col < self.size):
+            raise ValueError(
+                f"Row {row} and column {col} not within grid (grid size={self.size})")
+
+        self.board[row][col].state = state
+
+    def set_board_state(self, state: list[list[int]]):
+        """
+        Set the state of the board based off of a 2D list of values.
+
+        Raises an exception if dimensions of "state" do not match
+        the dimensions of this board.
+
+        Args:
+            state (list[list[int]]): the board state to copy as this board's state.
+        """
+        if len(state) != self.size:
+            raise ValueError("State must have have SIZE rows")
+
+        for row in range(0, self.size):
+            if len(state[row]) != self.size:
+                raise ValueError(
+                    f"All rows of `state` must have {self.size} rows. Row {row} of state is of length: {len(state[row])}")
+            for col in range(0, self.size):
+                self.board[row][col].state = state[row][col]
+
     def on_left_click(self, event):
         # print(f"Click at x: {event.x}, y: {event.y}", end="")
 
@@ -107,8 +181,8 @@ class Board:
             return
 
         # Flip cell from dead to alive.
-        cell.alive = 0 if cell.alive else 1
-        
+        cell.toggle()
+
         self.draw()
 
 
@@ -116,24 +190,118 @@ WIDTH = 800
 HEIGHT = 600
 SIZE = 3
 
+INPUT_DESCRIPTION = """\
+Use this grid to select the desired end state. Left click on cells to mark
+them as alive or dead. Once ready, use the submit button below to submit
+the query for a predecessor.
+""".replace("\n", " ")
+
+OUTPUT_DESCRIPTION = """\
+This grid visualizes the results of the query.
+""".replace("\n", " ")
+
 
 class App:
     """
     Top level application
     """
+
     def __init__(self):
         # (tenzin): Copying a good part of the Tkinter setup from CSE 493X lol
         self.window = tkinter.Tk()
-        self.board_canvas = tkinter.Canvas(
-            self.window, width=WIDTH, height=HEIGHT)
-        self.board_canvas.grid(column=0, row=0, padx=10, pady=10)
 
-        # Create internal board representation, have it take ownership of canvas.
-        self.board = Board(self.board_canvas, SIZE)
+        # App Grid layout. Coordinates are (row, col)
+        # Text for Input Board
+        # Input Frame (0, 0), Output Frame (0, 1)
+        # Submit button (1, 0)
 
-        # Store the Solver instance which will actually do the work
-        self.solver = Solver()
+        # Input frame and output frame:
+        # Title (0,0)
+        # Board (1, 0)
+        # Description (2, 0)
 
+        # ==== SETUP INPUT FRAME ====
+        self.input_frame = ttk.Frame(self.window)
+        self.input_frame.grid(row=0, column=0)
+
+        tkinter.Label(self.input_frame, text="Input").grid(row=0, column=0)
+        # Make board canvas. Set width and height to be required width/height for number of cells
+        # plus a bit extra.
+        self.input_board_canvas = tkinter.Canvas(
+            self.input_frame, width=(SIZE + 1) * CELL_SIZE, height=(SIZE + 1) * CELL_SIZE)
+        self.input_board_canvas.grid(row=1, column=0, padx=10, pady=10)
+        tkinter.Label(self.input_frame, text=INPUT_DESCRIPTION,
+                      wraplength=250, justify="left").grid(row=2, column=0, padx=10)
+
+        # ==== SETUP OUTPUT FRAME ====
+        self.output_frame = ttk.Frame(self.window)
+        self.output_frame.grid(row=0, column=1)
+
+        # Label
+        tkinter.Label(self.output_frame, text="Output").grid(row=0, column=0)
+
+        # Setup output board (for viewing results).
+        self.output_board_canvas = tkinter.Canvas(
+            self.output_frame, width=(SIZE + 1) * CELL_SIZE, height=(SIZE + 1) * CELL_SIZE)
+        self.output_board_canvas.grid(column=0, row=1, padx=10, pady=10)
+
+        # Description
+        tkinter.Label(self.output_frame, text=OUTPUT_DESCRIPTION,
+                      wraplength=250, justify="left").grid(row=2, column=0, padx=10)
+
+        # ==== Setup Internal State Logic ====
+        # Create internal board representation, have it take ownership of canvas (will handle drawing and
+        # updating based on input).
+        self.input_board: Board = Board(
+            self.input_board_canvas, SIZE, bind_callbacks=True)
+
+        self.output_board: Board = Board(
+            self.output_board_canvas, SIZE, bind_callbacks=False)
+
+        # ==== Add Button ====
+        self.submit_button = ttk.Button(
+            self.window, text="Get a valid predecessor!", command=self.submit_query)
+        self.submit_button.grid(row=1, column=0)
+
+    def constrain(self, solver: z3.Solver, slice: golz3.Slice):
+        """
+        Add constraints to require that the provided slice matches the current board state.
+
+        Args:
+            solver (Solver): The Z3 Solver to add constraints to.
+            vars (Slice): Set of variables representing cell states over time.
+        """
+
+        for row in range(0, SIZE):
+            for col in range(0, SIZE):
+                if self.input_board.get_cell_state(row, col):
+                    solver.add(slice[row][col] == 1)
+                else:
+                    # Must remember to constrain in the negative case too
+                    solver.add(slice[row][col] == 0)
+
+    def submit_query(self):
+        solver = z3.Solver()
+        state: list[golz3.Slice] = golz3.make_life(
+            solver=solver, grid_size=SIZE, time_steps=2)
+
+        # Constrain post state to match selected pattern.
+        self.constrain(solver, state[-1])
+
+        # Run the query
+        # print(solver.sexpr())
+        query_result = solver.check()
+        print(f"Query result: {query_result}")
+
+        # Print the model
+        model = solver.model()
+        golz3.print_model(model, state)
+
+        # Also visualize the first time step of model in output grid.
+        result = golz3.model_to_python(model, state)
+        self.output_board.set_board_state(result[0])
+        # Redraw board with updated state.
+        self.output_board.draw()
 
 if __name__ == '__main__':
     App()
